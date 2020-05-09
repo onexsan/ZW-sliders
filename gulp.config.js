c97d812d600153add9a70a85542746371675bb08
Nataliya <n.jelonkina@mail.ru>
2020-05-09 13:48:30 +0300

    start project

---
+++ gulp.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  SRC_PATH : "src",
+  DIST_PATH : "dist",
+  STYLES_LIBS: [
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/bxslider/dist/jquery.bxslider.css'
+
+  ],
+  JS_LIBS: [
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
+    'node_modules/bxslider/dist/jquery.bxslider.min.js'
+  ]
+}
