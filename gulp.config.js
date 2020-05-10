948e01c53c08820b2557353faac555b609149706
Victoria Filileeva <onexsan@gmail.com>
2020-05-10 15:45:24 +0300

    about page layout ready

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SRC_PATH : "src",
-  DIST_PATH : "dist",
+  SRC_PATH: "src",
+  DIST_PATH: "dist",
   STYLES_LIBS: [
     'node_modules/normalize.css/normalize.css',
     'node_modules/bxslider/dist/jquery.bxslider.css'
